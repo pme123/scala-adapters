09d2994d7d9303dc6925a94dedfd8d17bd2041f0
pme123 <feensturm@drachensegler.ch>
2018-03-15 19:28:15 +0100

    cosmetic

--- server/app/DemoModule.scala
+++ server/app/DemoModule.scala
@@ -7,10 +7,12 @@ import pme123.adapters.server.control.demo.DemoJobCreation
 class DemoModule extends AbstractModule with AkkaGuiceSupport {
 
   override def configure(): Unit = {
+    // init the JobCreation factory
     bind(classOf[JobCreation])
       .to(classOf[DemoJobCreation])
       .asEagerSingleton()
 
+    // you need to define the AccessControl
     bind(classOf[AccessControl])
       .to(classOf[NoAccessControl])
 
